a452c5c54e82838753aee286019b25f8e3b2ce5a
NightfallCoder <138189778+NightfallCoder@users.noreply.github.com>
2024-01-26 21:56:52 +0530

    Add files via upload

---
+++ stack_exp.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int postFixEvaluation(string s){
+    stack<int>st;
+    for(int i=0;i<s.length();i++){
+        if(s[i]>='0' && s[i]<='9'){
+            st.push(s[i]-'0');//converting to integer
+        }
+        else{
+            int op2=st.top();
+            st.pop();
+            int op1=st.top();
+            st.pop();
+            switch(s[i]){
+                case '+':
+                st.push(op1 + op2);
+                break;
+                case '-':
+                st.push(op1 - op2);
+                break;
+                case '/':
+                st.push(op1 / op2);
+                break;
+                case '*':
+                st.push(op1 * op2);
+                break;
+                case '%':
+                st.push(op1 % op2);
+                break;
+                case '^':
+                st.push(op1 ^ op2);
+                break;
+
+                default:
+                break;
+            }
+        }
+    }
+    return st.top();
+}
+int main(){
+    cout<<postFixEvaluation("46+2/5*7+");
+}
